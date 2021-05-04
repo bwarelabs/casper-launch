079ff5cb5d83be159c4e42d027c34defea899509
silviugae <71326712+silviugae@users.noreply.github.com>
2021-05-04 12:27:52 +0300

    TARGET_HOST is not hardcoded anymore

--- casper-validator/check_balance.sh
+++ casper-validator/check_balance.sh
@@ -26,14 +26,18 @@ TARGET_HOST="127.0.0.1"
 
 if nc -z -v localhost 8888; echo $? = 1
 then
-    TARGET_HOST="167.99.223.12"
+    KNOWN_ADDRESSES=$(sudo -u casper cat /etc/casper/$CASPER_VERSION/config.toml | grep known_addresses)
+    KNOWN_VALIDATOR_IPS=$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$KNOWN_ADDRESSES")
+    IFS=' ' read -r TARGET_HOST _REST <<< "$KNOWN_VALIDATOR_IPS"
+    echo "This is a known validator IP: $TARGET_HOST"
+    
     echo "Set Target Host as $TARGET_HOST, as localhost is not a casper-node."
 elif nc -z -v localhost 8888; echo $? = 0
 then
     TARGET_HOST="127.0.0.1"
 fi
 
-function GetPublicHEX() {
+function getPublicHex() {
 
     AutoHEX=$(cat "$public_hex_path")
 
@@ -50,23 +54,19 @@ function checkBalance() {
 echo && echo -e "${CYAN}Input HEX: ${GREEN}$INPUT_HEX${NC}" && echo
 
 LFB=$(curl -s http://$TARGET_HOST:8888/status | jq -r '.last_added_block_info | .height')
-
 echo -e "${CYAN}Chain height: ${GREEN}$LFB${NC}" && echo
 
 LFB_ROOT=$(casper-client get-block  --node-address http://$TARGET_HOST:7777 -b "$LFB" | jq -r '.result | .block | .header | .state_root_hash')
-
 echo -e "${CYAN}Block ${GREEN}$LFB ${CYAN}state root hash: ${GREEN}$LFB_ROOT${NC}" && echo
 
 PURSE_UREF=$(casper-client query-state --node-address http://$TARGET_HOST:7777 -k "$INPUT_HEX" -s "$LFB_ROOT" | jq -r '.result | .stored_value | .Account | .main_purse')
-
 echo -e "${CYAN}Main purse uref: ${GREEN}$PURSE_UREF${NC}" && echo
 
 BALANCE=$(casper-client get-balance --node-address http://$TARGET_HOST:7777 --purse-uref "$PURSE_UREF" --state-root-hash "$LFB_ROOT" | jq -r '.result | .balance_value')
-
 echo -e "${CYAN}Input balance: ${GREEN}$BALANCE${NC}" && echo
 
 }
 
-GetPublicHEX
+getPublicHex
 
 checkBalance
