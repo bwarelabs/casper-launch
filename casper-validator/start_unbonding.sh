d2f5c440abafbbba1c284ed2c5532df14261b901
silviugae@gmail.com <silviugae@gmail.com>
2021-05-14 19:02:10 +0000

    added delegate/undelegate scripts

--- casper-validator/start_unbonding.sh
+++ casper-validator/start_unbonding.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source .bonding_env
+source .unbonding_env
 
 PUBLIC_KEY_HEX=$(sudo -u casper cat /etc/casper/validator_keys/public_key_hex)
 STATE_ROOT_HASH=$(casper-client get-state-root-hash --node-address http://127.0.0.1:7777 | jq -r '.result | .state_root_hash')
@@ -10,13 +10,11 @@ casper-client get-balance --node-address http://127.0.0.1:7777 --purse-uref "$PU
 PUBLIC_KEY_HEX=$(sudo -u casper cat /etc/casper/validator_keys/public_key_hex)
 CHAIN_NAME=$(curl -s http://127.0.0.1:8888/status | jq -r '.chainspec_name')
 
-sudo -u casper casper-client put-deploy \
-	    --chain-name "$CHAIN_NAME" \
-	        --node-address "http://127.0.0.1:7777/" \
-		    --secret-key "/etc/casper/validator_keys/secret_key.pem" \
-		        --session-path "$HOME/casper-node/target/wasm32-unknown-unknown/release/add_bid.wasm" \
-			    --payment-amount 3000000000 \
-			        --gas-price=1 \
-				    --session-arg=public_key:"public_key='$PUBLIC_KEY_HEX'" \
-				        --session-arg=amount:"u512='${AMOUNT}'" \
-					    --session-arg=delegation_rate:"u8='${DELEGATION_RATE}'"
+casper-client put-deploy --chain-name "$CHAIN_NAME" \
+		         --node-address "http://127.0.0.1:7777" \
+			 --secret-key "/etc/casper/validator_keys/secret_key.pem" \
+			 --session-path  "$HOMEDIR/casper-node/target/wasm32-unknown-unknown/release/withdraw_bid.wasm"  \
+			 --payment-amount 1000000000  \
+			 --session-arg="public_key:public_key='$PUBLIC_KEY_HEX'" \
+			 --session-arg="amount:u512='$AMOUNT'" \
+			 --session-arg="unbond_purse:opt_uref=null"
